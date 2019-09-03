5d5378d96b9a87490a47a7f26e1ce29988abb1d0
Julien de Carufel <julien.decarufel@gmail.com>
2019-09-03 16:46:34 -0400

    initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <layout
+      :layout="layout"
+      direction="row"
+    />
+  </div>
+</template>
+
+<script>
+import Layout from './components/Layout.vue'
+
+const layout = [
+  {
+    component: Layout,
+    props: {
+      layout: [
+        {
+          component: Layout,
+          props: {
+            layout: [{ component: 'input' }],
+            direction: 'row'
+          }
+        }
+      ],
+      direction: 'column'
+    }
+  }
+]
+
+export default {
+  name: 'gin',
+  components: { Layout },
+  data () {
+    return { layout }
+  }
+}
+</script>
+
+<style>
+ #app {
+     height: 100%
+ }
+  /* CSS */
+</style>
