b1f7bc74b88c69e1b8c0eebf71ccac83773e5f90
decaruju <julien.decarufel@gmail.com>
2019-09-28 12:16:07 -0400

    sound app

---
+++ apps/sound/Sound.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="play">
+      play
+    </button>
+  </div>
+</template>
+
+<script>
+import baudio from 'baudio';
+
+export default {
+  name: 'Mail',
+  data() {
+    return {
+      client: undefined,
+      data: [],
+    };
+  },
+  methods: {
+    play() {
+      let i = 0, n = 0;
+      const b = baudio((t) => {
+        const x = Math.sin((t * 262) + Math.sin(n)); // eslint-disable-line no-magic-numbers
+        n += Math.sin(t);
+        i += 1;
+        if (i === 100000) throw Error; //eslint-disable-line no-magic-numbers
+        return x;
+      });
+      b.play();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
